9a571fa107ee7d5215e70a0177740382f5bcec07
Chloe Adams <chloeadams@Chloes-MacBook-Pro.local>
2017-08-07 16:38:52 -0500

    committing select changes

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,10 +5,13 @@ USE codeup_test_db;
 -- The name of all albums by Pink Floyd.
 SELECT * FROM albums WHERE artist = 'Pink Floyd';
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released
+select release_date from albums where name = 'Sgt. Pepper''s Lonely Hearts Club';
 -- The genre for Nevermind
+select genre from albums where name= 'Nevermind';
 -- Which albums were released in the 1990s
 SELECT name FROM albums where release_date between 1990 and 1999;
 -- Which albums had less than 20 million certified sales
+SELECT * FROM albums where sales < 20;
 -- All the albums in the rock genre. Is this all the rock albums in the table?
 select * from albums where genre like '%rock%';
 -- As always, commit your changes after each step and push them out to GitHub
