4309f4d8dcaf19449b53022663afc9c66371b196
Chloe Adams <chloeadams@Chloes-MacBook-Pro.local>
2017-08-08 15:47:32 -0500

    committing changes as of 3:47

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,23 +1,49 @@
 USE codeup_test_db;
 
--- Create a file called update_exercises.sql in your sql directory.
--- Write SELECT statements to output each of the following with a caption:
--- All albums in your table.
+
+-- SELECT QUERIES
+
+-- Select all albums in your table.
+	SELECT name AS 'All albums in the table'
+	FROM albums;
+
+-- Select all albums released before 1980
+	SELECT name AS 'albums released before 1980'
+	FROM albums
+	WHERE release_date < 1980;
+
+-- Select all albums by Michael Jackson
+	SELECT name AS 'albums by Michael Jackson'
+	FROM albums
+	WHERE artist = 'Michael Jackson';
+
+
+
+-- UPDATE QUERIES
+
 -- Make all the albums 10 times more popular (sales * 10)
-SELECT * from albums;
-update albums
-	set sales = * 10;
-	where 
--- All albums released before 1980
+-- Causes an error with current table constraints
+
+	UPDATE albums
+	SET sales = sales * 10;
+
+	SELECT * FROM albums;
+
+
 -- Move all the albums before 1980 back to the 1800s.
-SELECT name FROM albums where release_date < 1980;
-update albums
+-- Causes an error with current table constraints
 
--- All albums by Michael Jackson
--- Change 'Michael Jackson' to 'Peter Jackson'
-SELECT name FROM albums WHERE artist = 'Michael Jackson';
+	UPDATE albums
+	SET release_date = release_date - 100
+	WHERE release_date < 1980;
 
+-- Change "Michael Jackson" to "Peter Jackson"
 
--- Add SELECT statements after each UPDATE so you can see the results of your handiwork.
--- Push all your changes to GitHub.
+	
+	UPDATE albums
+	SET artist = 'Peter Jackson'
+	WHERE artist = 'Michael Jackson';
+
+	SELECT * FROM albums;
+
 
